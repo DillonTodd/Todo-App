06d0bec2a5241bd42b82c67cd02dbb997746900a
DillonTodd <98577108+DillonTodd@users.noreply.github.com>
2022-05-17 10:57:16 -0500

    add todo_list function view, and todo-list url path

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Todo
+from .serializers import TodoSerializer
 
 # Create your views here.
+@api_view(['GET'])
+def todo_list(request):
+    todos = Todo.objects.all()
+    serializer = TodoSerializer(todos)
+
+    return Response(serializer.data)
